7fcd45c537cffd54528a4b881af189dc24aec0ac
iPaw <pawizacked@gmail.com>
2025-10-21 08:04:35 -0600

    Initial Commit

---
+++ ChatitoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatitoTest
+//
+//  Created by iPaw on 21/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
